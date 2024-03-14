1d35eaf6cf42c113d214f66e33a6385f295def18
yasanthaniroshan <niroshanyi@gmail.com>
2024-03-14 10:18:28 +0530

    Added delay_us function

--- Lab_05/src/main.cpp
+++ Lab_05/src/main.cpp
@@ -9,30 +9,47 @@
 
 void delay_ms(int time)
 {
+    TCCR1B |= (1 << CS10) + (1 << CS11);
+    OCR1A = 250;
     TCNT1 = 0;
     int i = 0;
     while (i < time)
     {
         if (TIFR1 & (1 << OCF1A))
         {
-            i++;
             TCNT1 = 0;
             TIFR1 |= 1 << OCF1A;
+            i++;
         }
     }
     TCNT1 = 0;
 }
+void delay_us(long time)
+{
+    TCCR0B |= (1 << CS00);
+    OCR0A = 16;
+    long i = 0;
+    TCNT0 = 0;
+    while (i < time)
+    {
+        if (TCNT0 & (1 << OCF0A))
+        {
+            TCNT0 = 0;
+            TIFR0 |= 1 << OCF0A;
+            i++;
+        }
+    }
+    TCNT0 = 0;
+}
 
 int main()
 {
-    TCCR1B |= (1 << CS10) + (1 << CS11);
-    OCR1A = 250;
     DDRB |= 1 << LED_PIN;
     while (true)
     {
         PORTB |= 1 << LED_PIN;
-        delay_ms(1000);
+        delay_us(100000);
         PORTB = 0;
-        delay_ms(500);
+        delay_us(100000);
     }
 }
